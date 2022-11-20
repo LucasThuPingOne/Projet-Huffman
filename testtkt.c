68ef883b2c92edb2ae42fc7659676e37bd34b29c
Lucas <thupingone@LAPTOP-LDRDT03Q.localdomain>
2022-11-20 22:50:31 +0100

    clear history

---
+++ testtkt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+
+    char * firstName = (char *) malloc( 40 );
+    char * lastName = (char *) malloc( 40 );
+    char * fullName;
+    size_t fullSize;
+
+    printf( "Please, enter your first name: " );
+    scanf( "%s", firstName );
+
+    printf( "Enter your last name: " );
+    scanf( "%s", lastName );
+       
+    /* The last +1 is for the last nul ASCII code for the string */
+    fullSize = strlen( firstName ) + 1 +  strlen( lastName ) + 1;
+       
+    /* Memory allocation and copy */
+    fullName = (char *) malloc( fullSize );
+    strcpy( fullName, firstName );
+    strcat( fullName, " " );
+    strcat( fullName, lastName );
+    /* You can also write :
+    strcat( strcat( strcpy( fullName, firstName ), " " ), lastName );
+    */
+        
+    printf( "Full name is: %s\n", fullName );
+        
+    free( fullName );
+    free( lastName );
+    free( firstName );
+    
+    return 0;
+}
